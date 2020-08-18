f8b7ecacc8b3af8617f73a64af1e2c7cabf6b497
DannyM1chael <coldsoulrusirk@gmail.com>
2020-08-18 16:13:55 +0300

    added delete, ckeditor, auth

--- resources/views/posts/show.blade.php
+++ resources/views/posts/show.blade.php
@@ -1,11 +1,18 @@
 @extends('layouts.app')
 
 @section('content')
-    <a href="/lsapp/public/posts" class="btn btn-outline-dark mb-3">Go Back</a>
+    <a href="/lsapp/public/posts" class="btn btn-outline-dark mb-3" style="width: 100px">Go Back</a>
     <h1>{{ $post-> title }}</h1>
     <div>
-        {{ $post->body }}
+        {!! $post->body !!}
     </div>
     <hr>
     <small>Written on {{ $post->created_at }}</small>
+    <hr>
+    <a href="/lsapp/public/posts/{{ $post->id }}/edit" class="btn btn-outline-dark mb-3" style="width: 100px">Edit</a>
+
+    {!!Form::open(['action' => ['PostsController@destroy', $post->id], 'method' => 'POST', 'class' => 'float-right'])!!}
+        {{Form::hidden('_method', 'DELETE')}}
+        {{Form::submit('Delete', ['class' => 'btn btn-danger', 'style' => 'width:100px'])}}
+    {!!Form::close()!!}
 @endsection
